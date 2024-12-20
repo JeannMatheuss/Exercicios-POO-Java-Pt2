78efb6169bf66d013cab70d8ce3d7e41bf4bcfa2
Jean <jeanmatheus0612@gmail.com>
2024-12-19 21:33:12 -0300

    Adicionando Sets

---
+++ src/Principal1.java
@@ -0,0 +1,9 @@
+public class Principal1 {
+    public static void main(String[] args) {
+        ContaBancaria contaBancaria = new ContaBancaria();
+
+        contaBancaria.setNumeroConta(12345);
+        contaBancaria.setNomeTitular("João");
+        contaBancaria.setSaldo(1000.0);
+    }
+}
